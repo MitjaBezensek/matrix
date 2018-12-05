d62cba55257ecb8fd344b00b12e1628edef1c414
Mitja Bezenšek <mitja.bezensek@gmail.com>
2018-12-05 13:29:43 +0100

    Initial commit.

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var matrix2A0EB71A32564F22A1A6759C09DE0BF6 = {
+        name: 'matrix2A0EB71A32564F22A1A6759C09DE0BF6',
+        displayName: 'matrix',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '2.1.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.matrix2A0EB71A32564F22A1A6759C09DE0BF6.Visual(options),
+        custom: true
+    };
+}
